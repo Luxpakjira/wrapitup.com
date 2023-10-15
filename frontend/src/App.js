d46e0113c05cceedb285f4e89915dc72197e9dde
Luxpakjira <64940134+Luxpakjira@users.noreply.github.com>
2023-10-15 23:54:37 +0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,14 @@ import './index.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
-import { useNavigate } from "react-router-dom";
 
 function App() {
-  const navigate = useNavigate();
-  const handleClick = () => {
-    // 👇️ navigate programmatically
-    navigate('http://localhost:5000/login');
-  };
+
   return (
-    
       <div>
         <Routes>
-          <Route path="/" element={<Login />} />
-          <Route exact path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route exact path="/" element={<Home />} />
         </Routes>
       </div>
     
